927eadeeafa6d45a3f3abac3176950e7c7254f76
Diene89 <diene_89@hotmail.com>
2022-06-21 17:34:33 -0300

    Requisito 3 Ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const getTalkers = require('./middlewares/getTalkers');
 const getTalkerById = require('./middlewares/getTalkerById');
+const loginValidation = require('./middlewares/loginValidation');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,9 +13,11 @@ const PORT = '3000';
 // - Requisito 1
 app.get('/talker', getTalkers);
 
-
 // - Requisito 2
 app.get('/talker/:id', getTalkerById);
+
+// - Requisito 3
+app.post('/login', loginValidation);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ middlewares/loginValidation.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+
+const emailRegex = /\S+@\S+\.\S+/;
+
+const emailValidation = (email, res) => {
+    if (!email || email.length === 0) return res.status(400).json({"message": "O campo de email é obrigatório"});
+
+    if (!email.match(emailRegex)) return res.status(400).json({"message": "O email deve ter esse formato 'email@email.com"});
+}
+
+const passwordValidation = (password, res) => {
+    if (!password || password === 0) return res.status(400).json({"message": "O campo de password é obrigatório"});
+
+    if(password.length < 6) return res.status(400).json({"message": "O password deve possuir ao menos 6 caracteres"});
+}
+
+const generateToken = () => crypto.randomBytes(8).toString('hex');
+
+//  const generateToken = () => {
+//     const tokenRegex = new RegExp(/^[a-zA-Z0-9]{16}$/);
+//      return tokenRegex;
+// };
+//----------------------------------------------------------------------------------
+
+const loginValidation = async (req, res, next) => {
+    const { email, password } = req.body;
+    emailValidation(email, res);
+    passwordValidation(password, res);
+    const token = generateToken();
+    res.status(200).json({ token })
+}
+
+module.exports = loginValidation;
