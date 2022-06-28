8253fad3ee2035525a4f8f96ceae126565358cae
Diene89 <diene_89@hotmail.com>
2022-06-27 23:23:20 -0300

    lint corrigido

---
+++ middlewares/editTalker.js
@@ -0,0 +1,9 @@
+// const editTalk = async (req, res, next) => {
+//     const { id } = req.params;
+//     const { name, age, talk } = req.body;
+//     const talkers = JSON.parse(await fs.readFile('./talker.json'));
+//     const editTalk = talkers.find((talk) => talk.id === Number(id));
+//     if (!editTalk) {
+//         return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+//     return 
+// }
--- middlewares/getTalkerById.js
+++ middlewares/getTalkerById.js
@@ -5,8 +5,8 @@ const talkers = './talker.json';
 const getTalkerById = async (req, res) => {
     const { id } = req.params;
     const data = JSON.parse(await fs.readFile(talkers));
-    const talker = data.find((r) => r.id === Number(id))
-    if (!talker) return res.status(404).json({"message": "Pessoa palestrante não encontrada"});
+    const talker = data.find((r) => r.id === Number(id));
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
     
     res.status(200).json(talker);
 };
